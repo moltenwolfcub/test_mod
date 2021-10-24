9174039803caad30cbd1bcdec5a299ca15e5918c
Oliver Tomlins <oliver@tomlins.org.uk>
2021-10-24 19:03:03 +0100

    added cutom item group

--- src/main/java/moltenwolfcub/test/mod/TestMod.java
+++ src/main/java/moltenwolfcub/test/mod/TestMod.java
@@ -1,6 +1,7 @@
 package moltenwolfcub.test.mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
@@ -8,6 +9,7 @@ import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
@@ -20,12 +22,27 @@ public class TestMod implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger("test_mod_mwc");
 
 
+	public static final ItemGroup CUSTOM_GROUP = FabricItemGroupBuilder.create(
+		new Identifier("test_mod_mwc", "custom_page"))
+		.icon(() -> new ItemStack(TestMod.SLATE_PIECE))
+		.appendItems(stacks -> {
+			stacks.add(new ItemStack(TestMod.SLATE_PIECE));
+			stacks.add(new ItemStack(TestMod.SLATE_BLOCK));
+			stacks.add(new ItemStack(TestMod.SMOOTH_SLATE_BLOCK));
+			stacks.add(ItemStack.EMPTY);
+			stacks.add(new ItemStack(TestMod.DISCO_BLOCK));
+			stacks.add(new ItemStack(TestMod.NON_FRICTION_BLOCK));
+			stacks.add(ItemStack.EMPTY);
+		})
+		.build();
+
+
 	public static final Item SLATE_PIECE = new Item(new Item.Settings().group(ItemGroup.MISC).maxCount(64).rarity(Rarity.COMMON));
 
 	public static final Block SLATE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(2.25F, 6F).sounds(BlockSoundGroup.TUFF).breakByTool(FabricToolTags.PICKAXES).requiresTool());
 	public static final Block SMOOTH_SLATE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(2F, 6F).sounds(BlockSoundGroup.TUFF).breakByTool(FabricToolTags.PICKAXES).requiresTool());
 	public static final Block DISCO_BLOCK = new Block(FabricBlockSettings.of(Material.DECORATION).strength(1.5F, 1.4F).sounds(BlockSoundGroup.AMETHYST_BLOCK).breakByTool(FabricToolTags.PICKAXES).luminance(15));
-	public static final Block NON_FRICTION_BLOCK = new Block(FabricBlockSettings.of(Material.DECORATION).strength(0F, 0F).breakInstantly().slipperiness(100));
+	public static final Block NON_FRICTION_BLOCK = new Block(FabricBlockSettings.of(Material.DECORATION).strength(0F, 0F).breakInstantly().slipperiness(10));
 
 
 	@Override
